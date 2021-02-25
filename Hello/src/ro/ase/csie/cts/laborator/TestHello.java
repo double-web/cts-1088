33f43bc19b719fce63baefa45058b73f318e8d0d
Mihai Zinculescu <mihai.zinculescu@gmail.com>
2021-02-25 13:11:51 +0200

    something

---
+++ Hello/src/ro/ase/csie/cts/laborator/TestHello.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.laborator;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Salut");
+		
+		
+	}
+
+}
