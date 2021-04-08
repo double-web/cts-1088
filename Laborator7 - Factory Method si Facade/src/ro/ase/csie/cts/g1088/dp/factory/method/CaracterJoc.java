9850bf716c2ea4a5551f8ababe834307dcf13c07
Mihai Zinculescu <mihai.zinculescu@gmail.com>
2021-04-08 12:35:47 +0300

    inainte de pauza

---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterJoc.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+interface CaracterJoc {
+
+	public String nume = "";
+	public int putere = 100;
+	
+	public void seDeplaseaza();
+	
+}
---
+++ Laborator7 - Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterMarvel4Kids.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public class CaracterMarvel4Kids extends CaracterJoc {
+
+	@Override
+	public void seDeplaseaza() {}
+	
+}
